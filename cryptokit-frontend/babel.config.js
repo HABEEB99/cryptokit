f83fcf588117ac1d5e7fb73c56e8532d1398baba
Habeeb <habeebahmadu1@gmail.com>
2024-10-04 21:39:48 +0100

    feat: setup tailwind css

--- cryptokit-frontend/babel.config.js
+++ cryptokit-frontend/babel.config.js
@@ -1,13 +1,16 @@
 module.exports = {
   presets: ['module:@react-native/babel-preset'],
   plugins: [
-    ['module:react-native-dotenv', {
-      moduleName: '@env', // Import module as @env
-      path: '.env',       // Path to the .env file
-      safe: false,
-      allowUndefined: true,
-    }],
+    [
+      'module:react-native-dotenv',
+      {
+        moduleName: '@env', // Import module as @env
+        path: '.env', // Path to the .env file
+        safe: false,
+        allowUndefined: true,
+      },
+    ],
     'react-native-reanimated/plugin',
+    'nativewind/babel',
   ],
 };
-
